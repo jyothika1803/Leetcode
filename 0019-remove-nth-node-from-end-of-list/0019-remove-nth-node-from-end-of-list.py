cf6185facec7ea47cc700ff5232a3012714867e6
Jyothika Rajesh <jyothikarajesh1809@gmail.com>
2025-04-30 02:06:06 +0530

    Time: 0 ms (100%), Space: 17.8 MB (55.2%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.py
@@ -5,7 +5,7 @@
 #         self.next = next
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        dummy=ListNode(0)
+        dummy=ListNode()
         dummy.next=head
         first=second=dummy
         for i in range(n+1):
