e6178bb396f27b9f5e14af26fc79fc477e5fb4fd
Jyothika Rajesh <jyothikarajesh1809@gmail.com>
2025-08-07 00:37:58 +0530

    Time: 3 ms (62.46%), Space: 18.4 MB (29.27%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        trips.sort(key= lambda x:x[1])
+        current_passengers=0
+        min_heap=[]
+        for num,start,end in trips:
+            while min_heap and min_heap[0][0]<=start:
+                drop,leaving=heapq.heappop(min_heap)
+                current_passengers-=leaving
+            current_passengers+=num
+            if current_passengers>capacity:
+                return False
+            heapq.heappush(min_heap,(end,num))
+        return True
