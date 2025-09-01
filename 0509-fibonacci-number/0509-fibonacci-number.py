a156ab61918f7f8ccb8ffadae3fbbe9d3f589b34
Jyothika Rajesh <jyothikarajesh1809@gmail.com>
2025-09-01 16:56:00 +0530

    Time: 39 ms (64.5%), Space: 17.8 MB (23.15%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -7,22 +7,6 @@ class Solution:
         dp[1]=1
         for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
-        return dp[n] 
-        
-        """
-        #recursion
-        if n<=1:
-            return n
-        return self.fib(n-1)+self.fib(n-2)
-        """
+        return dp[n]
 
-        """
-        #iterative
-        if n<=1:
-            return n
-        a=0
-        b=1
-        for _ in range(2,n+1):
-            a,b=b,a+b #simultaneous update
-        return b
-        """
+        
