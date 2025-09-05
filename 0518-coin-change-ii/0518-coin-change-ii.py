2187a0ac18e1d45abf585cd22c1cd82f4f757cfe
Jyothika Rajesh <jyothikarajesh1809@gmail.com>
2025-09-06 00:55:08 +0530

    Time: 564 ms (21.97%), Space: 39.3 MB (45.24%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,10 +1,21 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        dp=[0]*(amount+1)
-        dp[0]=1
-        for coin in coins:
-            for amt in range(coin, amount + 1):
-                dp[amt]+=dp[amt-coin]
-            
-        return dp[amount]
+        n=len(coins)
+        dp=[]
+        for _ in range(n+1):    
+            row=[-1]*(amount+1)   
+            dp.append(row)
+        def helper(amount,index):
+            if amount==0:
+                return 1
+            if amount<0 or index>=n:
+                return 0
+            if dp[index][amount]!=-1:
+                return dp[index][amount]
+
+            take=helper(amount-coins[index],index)
+            skip=helper(amount,index+1)
+            dp[index][amount]=take+skip
+            return dp[index][amount]
+        return helper(amount,0)
         
