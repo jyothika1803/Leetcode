56bbc62404e6c81682e733b21f9ddcd7ef480fe1
Jyothika Rajesh <jyothikarajesh1809@gmail.com>
2025-11-05 23:03:19 +0530

    Time: 2 ms (52.83%), Space: 17.9 MB (20.22%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.py
@@ -1,12 +1,10 @@
 class Solution:
     def countTestedDevices(self, batteryPercentages: List[int]) -> int:
-        sub=[1]*len(batteryPercentages)
         count=0
-        for i in range(len(batteryPercentages)):
-            if batteryPercentages[i]==0:
-                continue
-            else:
+        decrement=0
+        for battery in batteryPercentages:
+            curr=battery-decrement
+            if curr>0:
                 count+=1
-                for j in range(i+1,len(batteryPercentages)):
-                    batteryPercentages[j]=max(0,batteryPercentages[j]-1)
+                decrement+=1
         return count
